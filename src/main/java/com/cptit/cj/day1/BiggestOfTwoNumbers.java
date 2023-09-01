4ce748fca05d7ed8532f2c42159e6781645b9c06
vijaya lakshmi <vxk23230@ucmo.edu>
2023-08-31 20:33:59 -0500

    Added biggest of two numbers program

---
+++ src/main/java/com/cptit/cj/day1/BiggestOfTwoNumbers.java
@@ -0,0 +1,10 @@
+package com.cptit.cj.day1;
+
+public class BiggestOfTwoNumbers {
+
+    public static void main(String[] args){
+        int a = 10;
+        int b = 20;
+        System.out.println("Biggest is :" + (a > b ? a : b));
+    }
+}
