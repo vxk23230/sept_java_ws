cb82210da8a3f2dfceb0ec728f33ca77c6384ff5
vijaya lakshmi <vxk23230@ucmo.edu>
2023-08-31 20:26:14 -0500

    First commit of Sept batch

---
+++ src/main/java/com/cptit/cj/Main.java
@@ -0,0 +1,7 @@
+package com.cptit.cj;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ src/main/java/com/cptit/cj/day1/SumOfTwoNumbers.java
@@ -0,0 +1,12 @@
+package com.cptit.cj.day1;
+
+public class SumOfTwoNumbers {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        int sum = a + b;
+        System.out.println(a+" + "+b+" = "+sum);
+
+    }
+
+    }
